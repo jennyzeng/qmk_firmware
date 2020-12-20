278ff05407eb4a1a3f5badd9b9d0763142a59e77
Jenny Zeng <jennyzengzzh@gmail.com>
2020-12-20 14:16:19 -0800

    add mouse layer

--- keyboards/atreus62/keymaps/jenny/keymap.c
+++ keyboards/atreus62/keymaps/jenny/keymap.c
@@ -21,20 +21,21 @@
 #define _ANDROID 2
 #define _NUMPAD 3
 #define _MEDIA 4
+#define _MOUSE 5
 
 #define TO_DEFAULT TO(_DEFAULT)
 #define FN MO(_FN)
 #define MO_MEDIA MO(_MEDIA)
-#define TG_NUMPAD TG(_NUMPAD)
+#define TG_MOUSE TG(_MOUSE)
 #define MO_NUMPAD MO(_NUMPAD)
 #define OSL_ANDROID OSL(_ANDROID)
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_DEFAULT] = LAYOUT(KC_ESCAPE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, TG_NUMPAD, MO_MEDIA, MO_NUMPAD, KC_LALT, KC_LGUI, KC_SPC, FN, KC_ENT, KC_SPC, OSL_ANDROID, KC_EQL, KC_MINS, KC_LBRC, KC_RBRC),
-                                                              [_FN]      = LAYOUT(KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_NO, KC_UNDERSCORE, KC_KP_PLUS, KC_MINS, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_NO, KC_NO, KC_F12, _____, KC_NO, KC_KP_SLASH, KC_KP_ASTERISK, KC_PEQL, KC_LBRACKET, KC_LEFT_ANGLE_BRACKET, KC_RIGHT_ANGLE_BRACKET, KC_RBRACKET, KC_NO, KC_NO, KC_NO, KC_NO, _____, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _____, _____, _____, _____, _____, _____, _____, _____, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_DEFAULT] = LAYOUT(KC_ESCAPE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, TG_MOUSE, MO_MEDIA, KC_LALT, MO_NUMPAD, KC_LGUI, KC_SPC, FN, KC_ENT, KC_SPC, OSL_ANDROID, KC_EQL, KC_MINS, KC_LBRC, KC_RBRC),
+                                                              [_FN]      = LAYOUT(KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_NO, KC_UNDERSCORE, KC_KP_PLUS, KC_MINS, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_NO, KC_NO, KC_F12, _____, KC_NO, KC_KP_SLASH, KC_KP_ASTERISK, KC_PEQL, KC_LBRACKET, KC_LEFT_ANGLE_BRACKET, KC_RIGHT_ANGLE_BRACKET, KC_RBRACKET, KC_NO, KC_NO, KC_NO, KC_NO, _____, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _____, _____, _____, _____, _____, _____, _____, _____, KC_NO, KC_NO, KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT),
                                                               [_ANDROID] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _IMPORT, _FORMAT, KC_NO, KC_NO, _ACTION, _SCREEN_SHOT, _DUXO, _FRAGMENT, KC_NO, KC_NO, KC_NO, KC_NO, _GT_LAYOUT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _VIEW_STATE, KC_NO, KC_NO, KC_NO, KC_NO, _RENAME, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO_DEFAULT, KC_NO, _REFACTOR, KC_NO, KC_NO),
-                                                              [_NUMPAD]  = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSLS, KC_PAST, KC_PMNS, KC_NO, KC_NO, _____, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_NO, _____, _____, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_P4, KC_P5, KC_P6, KC_PCMM, KC_NO, KC_NO, _____, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_NO, KC_NO, _____, _____, _____, _____, _____, _____, _____, _____, _____, KC_P0, KC_PDOT, KC_PENT, KC_NO, KC_NO),
-                                                              [_MEDIA]   = LAYOUT(_INPUT_M, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_VOLU, KC_NO, KC_NO, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_NO, KC_NO, KC_NO, KC_MRWD, KC_VOLD, KC_MFFD, KC_NO, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, KC_NO, KC_NO, _____, _____, _____, _____, _____, KC_MPLY, RGB_TOG, RGB_M_X, RGB_M_G, RGB_M_K, KC_NO, KC_NO, KC_NO)};
-
+                                                              [_NUMPAD]  = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _____, KC_1, KC_2, KC_3, KC_4, KC_5, KC_KP_PLUS, KC_KP_MINUS, KC_KP_ASTERISK, KC_KP_SLASH, KC_NO, _____, _____, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PCMM, KC_PEQL, KC_PDOT, KC_NO, KC_NO, KC_NO, _____, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, KC_NO, KC_NO, KC_NO, KC_NO),
+                                                              [_MEDIA]   = LAYOUT(_INPUT_M, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_VOLU, KC_NO, KC_NO, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_NO, KC_NO, KC_NO, KC_MRWD, KC_VOLD, KC_MFFD, KC_NO, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, KC_NO, _____, _____, _____, _____, _____, _____, KC_MPLY, RGB_TOG, RGB_M_X, RGB_M_G, RGB_M_K, KC_NO, KC_NO, KC_NO),
+                                                              [_MOUSE]   = LAYOUT(KC_NO, KC_ACL0, KC_ACL1, KC_ACL2, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_D, KC_NO, KC_NO, KC_NO, KC_NO, KC_MS_U, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_R, KC_WH_U, KC_WH_L, KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _____, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN2, KC_BTN1, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)};
 
 uint8_t  prev = _DEFAULT;
 uint32_t check;
@@ -59,7 +60,8 @@ uint32_t layer_state_set_user(uint32_t state) {
                 break;
             case _NUMPAD:
                 rgblight_mode(RGBLIGHT_MODE_TWINKLE);
-
+            case _MOUSE:
+                rgblight_mode(RGBLIGHT_MODE_KNIGHT + 1);
             case _MEDIA:
                 break;
         }
